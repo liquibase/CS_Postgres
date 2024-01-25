1cddee539dde3351e722430f27974a799b13c4ff
Amy Smith <asmith@liquibase.com>
2024-01-25 16:20:03 -0600

    Adding table 4

---
+++ src/main/resources/changelogs/v0.1.0/test_table_04.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset amy.smith:test_table_04 labels:v0.1.0
+CREATE TABLE abc.new_table_04 (
+	test_id int4 NOT NULL,
+	test_column varchar NULL,
+	CONSTRAINT test_table_pkey PRIMARY KEY (test_id)
+);
+--rollback DROP TABLE abc.new_table_04;
