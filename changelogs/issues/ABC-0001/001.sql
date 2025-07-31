ab3f298c6937a5fc9fce5e1e78cb98c583ac951a
Amy Smith <asmith@liquibase.com>
2025-07-31 12:04:54 -0500

    Push

--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -1,36 +1,9 @@
 --liquibase formatted sql
 
---changeset amy.smith:sales_contacts labels:v0.1.0,abc-0001
-create table Sales.Contacts2 (
+--changeset amy.smith:contacts
+create table Contacts (
   id int, 
   name varchar(30),
   dept varchar(30) 
 );
---rollback DROP TABLE Sales.Contacts2;
-
-
---changeset amy.smith:sales_contacts_idx labels:v0.1.0,abc-0001
-CREATE UNIQUE INDEX name2_idx ON Sales.Contacts2 (name);
---rollback DROP INDEX name2_idx on Sales.Contacts2;
-
---changeset amy.smith:stores labels:abc-0001 
-create table Sales.Stores2 (
-  id int, 
-  name varchar(30),
-  city varchar(30) 
-);
---rollback DROP TABLE Sales.Stores2;
-
---changeset amy.smith:goals labels:abc-0001 
-create table Sales.Goals2 (
-  id int, 
-  name varchar(30) 
-);
---rollback DROP TABLE Sales.Goals2;
-
---changeset amy.smith:kpis labels:abc-0001 
-create table Sales.Kpis2 (
-  id int, 
-  name varchar(30) 
-);
---rollback DROP TABLE Sales.Kpis2;
+--rollback DROP TABLE Contacts;
