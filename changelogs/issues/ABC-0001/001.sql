8e17bba865285afa6a6975012d9db097e331f3ea
Amy Smith <asmith@liquibase.com>
2025-07-31 14:43:50 -0500

    Another test

--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -7,3 +7,11 @@ create table Contacts (
   dept varchar(30) 
 );
 --rollback DROP TABLE Contacts;
+
+--changeset amy.smith:contacts2
+create table Contacts2 (
+  id int, 
+  name varchar(30),
+  dept varchar(30) 
+);
+--rollback DROP TABLE Contacts2;
