76593e9a0f2c157ef80860efece1f1fc94fe0dde
daticalamy <50338582+daticalamy@users.noreply.github.com>
2025-08-05 08:48:00 -0500

    Update 001.sql

--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -16,14 +16,6 @@ create table Contacts2 (
 );
 --rollback DROP TABLE Contacts2;
 
---changeset amy.smith:contacts_delete
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:contacts_delete2
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:contacts_delete3
+--changeset amy.smith:contacts_delete_from_1
 delete from Contacts;
 --rollback select '1';
