bdddc75eba1cf2d6b5bdc5674143e573c83047a6
Amy Smith <asmith@liquibase.com>
2025-08-13 10:59:40 -0500

    Updated changesets

--- changelogs/issues/ABC-0002/001.sql
+++ changelogs/issues/ABC-0002/001.sql
@@ -1,11 +1,15 @@
 --liquibase formatted sql
 
 --changeset amy.smith:sales_contacts labels:abc-002
-create table Sales.Contacts2 (
+create table Contacts2 (
   id int
 );
---rollback DROP TABLE Sales.Contacts2;
+--rollback DROP TABLE Contacts2;
 
 --changeset amy.smith:sales_contacts_idx labels:abc-002
-CREATE UNIQUE INDEX name2_idx ON Sales.Contacts2 (name);
---rollback DROP INDEX name2_idx on Sales.Contacts2;
+CREATE UNIQUE INDEX name2_idx ON Contacts2 (name);
+--rollback DROP INDEX name2_idx on Contacts2;
+
+--changeset amy.smith:Contacts2_delete labels:abc-0002 runAlways:true
+delete from Contacts2;
+--rollback select '1';
