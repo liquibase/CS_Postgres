5b73b9c0176f52a7bf38f8e8a8fa6afd80f7e1f9
daticalamy <50338582+daticalamy@users.noreply.github.com>
2025-08-14 07:47:36 -0500

    Update 001.sql

--- changelogs/issues/ABC-0002/001.sql
+++ changelogs/issues/ABC-0002/001.sql
@@ -12,6 +12,10 @@ create table Contacts4 (
 CREATE UNIQUE INDEX name4_idx ON Contacts4 (name);
 --rollback DROP INDEX name4_idx on Contacts4;
 
---changeset amy.smith:Contacts4_delete labels:abc-0002 runAlways:true
+--changeset amy.smith:Contacts4_delete labels:abc-0002
 delete from Contacts4;
+--rollback select '1';
+
+--changeset amy.smith:Contacts4_insert labels:abc-0002 runAlways=true
+insert into Contacts4 (id, name, dept) values (1,'Name A','Dept A') ;
 --rollback select '1';
