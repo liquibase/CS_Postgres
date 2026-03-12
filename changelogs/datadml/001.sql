3f11d4d593655b705fc64536fb01a8c04909b83d
Amy Smith <asmith@liquibase.com>
2026-03-12 13:44:02 -0500

    DML

--- changelogs/datadml/001.sql
+++ changelogs/datadml/001.sql
@@ -5,6 +5,24 @@ insert into contacts (id, name, dept) values (1, 'CS', '123456');
 insert into contacts (id, name, dept) values (2, 'Finance', '123457');
 --rollback delete from contacts where id in (1,2);
 
---changeset amy.smith:contacts_update
+--changeset amy.smith:contacts_update runAlways:true runOnChange:true
+\echo update contacts set dept = 'Finance Serv' where id = 2;
 update contacts set dept = 'Finance Serv' where id = 2;
 --rollback update contacts set dept = 'Finance' where id = 2;
+
+--changeset amy.smith:contacts_delete runAlways:true runOnChange:true
+\echo delete from contacts where id in (1,2);
+delete from contacts where id in (1,2);
+--rollback insert into contacts (id, name, dept) values (1, 'CS', '123456');
+--rollback insert into contacts (id, name, dept) values (2, 'Finance', '123457');
+
+--changeset amy.smith:contacts_insert2 runAlways:true runOnChange:true
+\echo Inserting...
+insert into contacts (id, name, dept) values (1, 'CS', '123456');
+insert into contacts (id, name, dept) values (2, 'Finance', '123457');
+--rollback delete from contacts where id in (1,2);
+
+--changeset amy.smith:contacts_select runAlways:true runOnChange:true
+\echo select id, name, dept from contacts;
+select id, name, dept from contacts;
+--rollback select id, name, dept from contacts;
--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -15,27 +15,3 @@ create table Contacts2 (
   dept varchar(30) 
 );
 --rollback DROP TABLE Contacts2;
-
---changeset amy.smith:contacts_delete_from_1 runAlways:true labels:abc-0001 
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:contacts_delete_from_2
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:contacts_delete_from_3 labels:abc-0001 
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:1 labels:@test
-delete from Contacts;
---rollback select '1';
-
---changeset amy.smith:2 
-delete from Contacts;
---rollback select '2';
-
---changeset amy.smith:3 
-delete from Contacts;
---rollback select '3';
