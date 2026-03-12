6e9a9d6a50c7e4687d5375aae54d38c8208108cc
Amy Smith <asmith@liquibase.com>
2026-03-12 16:56:51 -0500

    Added jdbc changes

--- changelogs/issues/ABC-0001/001.sql
+++ changelogs/issues/ABC-0001/001.sql
@@ -15,3 +15,12 @@ create table Contacts2 (
   dept varchar(30) 
 );
 --rollback DROP TABLE Contacts2;
+
+--changeset amy.smith:contacts_insert
+insert into contacts (id, name, dept) values (100, 'CS', '123456');
+insert into contacts (id, name, dept) values (200, 'Finance', '123457');
+--rollback delete from contacts where id in (100,200);
+
+--changeset amy.smith:contacts_update runAlways:true runOnChange:true
+update contacts set dept = 'Blah';
+--rollback
